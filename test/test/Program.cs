d2bc0e05c2eafa3e1710e95a41e385d9cc9004d8
s-sepehr-razavi <s.sepehr.razavi@gmail.com>
2022-11-16 21:44:41 +0330

    feature_1 second

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -7,6 +7,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             int feature_1 = 0;
+            feature_1++;
         }
     }
 }
